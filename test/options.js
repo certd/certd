a39dac4dbd392a6a7c2ffe98086116ad3f357ceb
xiaojunnuo <xiaojunnuo@qq.com>
2021-02-04 11:17:54 +0800

    refactor: rename ui

--- test/options.js
+++ test/options.js
@@ -48,7 +48,7 @@ const defaultOptions = {
   deploy: [
     {
       deployName: '流程1-部署到阿里云系列产品',
-      disabled: true,
+      disabled: false,
       tasks: [
         {
           taskName: '上传证书到云',
@@ -94,7 +94,7 @@ const defaultOptions = {
     },
     {
       deployName: '流程3-触发jenkins任务',
-      disabled: true,
+      disabled: false,
       tasks: [
         {
           taskName: '触发jenkins任务',
@@ -108,6 +108,7 @@ const defaultOptions = {
     },
     {
       deployName: '流程4-部署到腾讯云ingress',
+      disabled: true,
       tasks: [
         {
           taskName: '上传到腾讯云',
