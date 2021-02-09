7326119f528252dcd1d7af56e0c7c9261833c63d
xiaojunnuo <xiaojunnuo@qq.com>
2021-02-09 21:07:19 +0800

    refactor: export

--- packages/ui/certd-ui/src/icons.js
+++ packages/ui/certd-ui/src/icons.js
@@ -9,19 +9,19 @@ import {
   DeleteOutlined
 } from '@ant-design/icons-vue'
 
+// 必须这么写。否则打包正式环境的时候显示不了图标
 const icons = {
-  PlusCircleOutlined,
-  PlusOutlined,
-  CheckOutlined,
-  EditOutlined,
-  ArrowRightOutlined,
-  NodeIndexOutlined,
-  ThunderboltOutlined,
-  DeleteOutlined
+  PlusCircleOutlined: PlusCircleOutlined,
+  PlusOutlined: PlusOutlined,
+  CheckOutlined: CheckOutlined,
+  EditOutlined: EditOutlined,
+  ArrowRightOutlined: ArrowRightOutlined,
+  NodeIndexOutlined: NodeIndexOutlined,
+  ThunderboltOutlined: ThunderboltOutlined,
+  DeleteOutlined: DeleteOutlined
 }
 export default function (app) {
-  _.forEach(icons, item => {
-    console.log('icons:', item.name)
-    app.component(item.name, item)
+  _.forEach(icons, (item, key) => {
+    app.component(key, item)
   })
 }
