b1181c0f9a03b632fd0da1a2cd0fc67515d06713
xiaojunnuo <xiaojunnuo@qq.com>
2021-12-04 18:39:05 +0800

    refactor: 1

--- packages/plugins/plugin-aliyun/src/index.js
+++ packages/plugins/plugin-aliyun/src/index.js
@@ -1,16 +1,17 @@
-
 import _ from 'lodash-es'
 
 import { AliyunDnsProvider } from './dns-providers/aliyun.js'
 import { AliyunAccessProvider } from './access-providers/aliyun.js'
 import { UploadCertToAliyun } from './plugins/upload-to-aliyun/index.js'
 import { DeployCertToAliyunCDN } from './plugins/deploy-to-cdn/index.js'
+import { DeployCertToAliyunAckIngress } from './plugins/deploy-to-ack-ingress/index.js'
 
 import { pluginRegistry, accessProviderRegistry, dnsProviderRegistry } from '@certd/api'
 
 export const Plugins = {
   UploadCertToAliyun,
-  DeployCertToAliyunCDN
+  DeployCertToAliyunCDN,
+  DeployCertToAliyunAckIngress
 }
 export default {
   install () {
