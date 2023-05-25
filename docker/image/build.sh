8498fa75e3985a22cbed641bebb8d0d7217f4291
xiaojunnuo <xiaojunnuo@qq.com>
2023-05-25 16:13:55 +0800

    chore: pre publish

--- docker/image/build.sh
+++ docker/image/build.sh
@@ -7,19 +7,19 @@ echo "您输入的版本号是： $version"
 echo "登录aliyun镜像仓库"
 docker login --username=252959493@qq.com registry.cn-shenzhen.aliyuncs.com
 
-$build=$(pwd)
+build=$(pwd)
 cd ../../
-$root=$(pwd)
+root=$(pwd)
 echo "安装依赖"
 pnpm install
 
 echo "client build"
-cd $root/packages/certd-client
+cd $root/packages/ui/certd-client
 pnpm run build
 echo "client build success"
 
 echo "server build"
-cd $root/packages/certd-server
+cd $root/packages/ui/certd-server
 pnpm run build
 echo "server build success"
 
@@ -27,6 +27,7 @@ echo "rm node_modules"
 rm ./node_modules -rf
 
 echo "copy to workspace"
+mkdir -p $build/workspace/certd-server
 \cp ./* $build/workspace/certd-server -rf
 \cp ../certd-client/dist/* $build/workspace/certd-server/public/ -rf
 
