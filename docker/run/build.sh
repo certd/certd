b5a8a9e08a7084d6c714e1f09136c451c25b1e8a
xiaojunnuo <xiaojunnuo@qq.com>
2023-07-03 10:05:50 +0800

    chore: deploy

--- docker/run/build.sh
+++ docker/run/build.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 set -e
-echo "请先输入一个版本号(如 1.0.6)："
-read version
+if $CERTD_VERSION; then
+    echo "CERTD_VERSION is set = $CERTD_VERSION"
+    version=$CERTD_VERSION
+else
+    echo "CERTD_VERSION is not set"
+    echo "请先输入一个版本号(如 1.0.6)："
+    read version
+fi
+
 echo "您输入的版本号是： $version"
 export TAG="$version"
 sudo -E docker compose up -d
