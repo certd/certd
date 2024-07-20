1c6740feff9dc4e118e8ea69b2e646fa7a7ee77c
xiaojunnuo <xiaojunnuo@qq.com>
2024-07-20 18:07:32 +0800

    chore: sqlite换成better-sqlite

--- packages/ui/certd-server/db/migration/v10005__password2.sql
+++ packages/ui/certd-server/db/migration/v10005__password2.sql
@@ -1,4 +1,4 @@
-INSERT INTO sys_settings ("key", title, setting,access) VALUES ("sys.install","安装信息",'{"installTime":'|| (select timestamp from 'flyway_history' where id = 1 )||'}',"private");
+INSERT INTO sys_settings ("key", title, setting,access) VALUES ('sys.install','安装信息','{"installTime":'|| (select timestamp from 'flyway_history' where id = 1 )||'}','private');
 
 ALTER TABLE sys_user ADD COLUMN password_version integer DEFAULT 1;
 ALTER TABLE sys_user ADD COLUMN password_salt varchar(36);
