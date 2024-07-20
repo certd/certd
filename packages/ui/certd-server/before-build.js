153e98b593d80ab5ddc268f334eaadfee1719eaf
xiaojunnuo <xiaojunnuo@qq.com>
2024-07-20 14:26:01 +0800

    chore: 1

---
+++ packages/ui/certd-server/before-build.js
@@ -0,0 +1,6 @@
+import http from 'axios';
+import fs from 'fs';
+//读取 packages/core/pipline/package.json的版本号
+import { default as packageJson } from './tsconfig.json' assert { type: 'json' };
+delete packageJson.references;
+fs.writeFileSync('./tsconfig.json', JSON.stringify(packageJson, null, 2));
