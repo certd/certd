741172fd98cf68338e5f8c47037a8e3f48c88df2
xiaojunnuo <xiaojunnuo@qq.com>
2024-07-24 02:16:12 +0800

    chore:

--- publish-check.js
+++ publish-check.js
@@ -1,5 +1,6 @@
 
 import fs from 'fs'
+import childProcess from 'child_process';
 function check(){
     const gitAdd =  fs.readFileSync("./node_modules/@lerna-lite/version/dist/lib/git-add.js","utf-8")
     if(gitAdd.indexOf("('git', ['add', '.']") > -1){
@@ -10,7 +11,13 @@ function check(){
     }
 
     //GH_TOKEN
+    console.log("检查 GH_TOKEN")
+    if(process.env.npm_config_gh_token){
+        process.env.GH_TOKEN = process.env.npm_config_gh_token
+        childProcess.exec("set GH_TOKEN="+process.env.npm_config_gh_token)
+    }
     if(!process.env.GH_TOKEN){
+        console.log(process.env)
         console.error("GH_TOKEN 未设置")
         throw new Error("GH_TOKEN 未设置")
     }
