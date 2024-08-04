0227155ab40371c533c9a3c4f5736203a3d657b3
xiaojunnuo <xiaojunnuo@qq.com>
2024-08-04 22:46:08 +0800

    chore:

--- packages/ui/certd-server/src/config/keys.ts
+++ packages/ui/certd-server/src/config/keys.ts
@@ -2,6 +2,7 @@ import fs from 'fs';
 import yaml from 'js-yaml';
 import * as _ from 'lodash-es';
 import { nanoid } from 'nanoid';
+import path from 'path';
 const KEYS_FILE = './data/keys.yaml';
 export class Keys {
   jwtKey: string = nanoid();
@@ -19,6 +20,12 @@ export class Keys {
   }
 
   save() {
+    const parent = path.dirname(KEYS_FILE);
+    if (!fs.existsSync(parent)) {
+      fs.mkdirSync(parent, {
+        recursive: true,
+      });
+    }
     fs.writeFileSync(KEYS_FILE, yaml.dump(this));
   }
 }
