8851870400df86e496198ad509061b8989fcc44f
xiaojunnuo <xiaojunnuo@qq.com>
2024-10-14 13:30:50 +0800

    fix: 修复版本号获取错误的bug

--- packages/ui/certd-server/src/utils/version.ts
+++ packages/ui/certd-server/src/utils/version.ts
@@ -1,7 +1,13 @@
+import { logger } from '@certd/pipeline';
 import fs from 'fs';
 
 export async function getVersion() {
-  const pkg = await fs.promises.readFile('../../../package.json');
-  const pkgJson = JSON.parse(pkg.toString());
-  return pkgJson.version;
+  try {
+    const pkg = await fs.promises.readFile('./package.json');
+    const pkgJson = JSON.parse(pkg.toString());
+    return pkgJson.version;
+  } catch (e) {
+    logger.error(e);
+    return 'unknown';
+  }
 }
