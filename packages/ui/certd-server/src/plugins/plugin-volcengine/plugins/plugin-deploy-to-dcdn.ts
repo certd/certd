cf008b9cff54542dc551fb159578dbd49006229c
xiaojunnuo <xiaojunnuo@qq.com>
2026-08-06 11:57:40 +0800

    fix: 优化火山引擎dcdn，单个单个部署，规避超时问题

--- packages/ui/certd-server/src/plugins/plugin-volcengine/plugins/plugin-deploy-to-dcdn.ts
+++ packages/ui/certd-server/src/plugins/plugin-volcengine/plugins/plugin-deploy-to-dcdn.ts
@@ -103,6 +103,9 @@ export class VolcengineDeployToDCDN extends AbstractTaskPlugin {
     this.certDomains = new CertReader(this.cert).getAllDomains();
 
     let domainList = this.domainList;
+    if (typeof domainList === "string") {
+      domainList = [domainList];
+    }
     if (!this.autoMatch) {
       //手动根据域名部署
       if (!this.domainList || this.domainList.length === 0) {
@@ -129,8 +132,10 @@ export class VolcengineDeployToDCDN extends AbstractTaskPlugin {
     }
 
     //域名十个十个的分割
-    for (let i = 0; i < domainList.length; i += 10) {
-      const batch = domainList.slice(i, i + 10);
+    const splitSize = 2;
+
+    for (let i = 0; i < domainList.length; i += splitSize) {
+      const batch = domainList.slice(i, i + splitSize);
       this.logger.info(`开始部署证书到域名:${batch}`);
       const res = await service.request({
         action: "CreateCertBind",
@@ -143,6 +148,7 @@ export class VolcengineDeployToDCDN extends AbstractTaskPlugin {
         version: "2021-04-01",
       });
       this.logger.info(`部署证书到域名成功：`, JSON.stringify(res));
+      await this.ctx.utils.sleep(2000);
     }
 
     this.logger.info("部署完成");
